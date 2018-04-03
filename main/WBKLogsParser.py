3c130b897c4cf4b8ebcc69937b598b31f40acfe4
PiotrFalkiewicz <piotrfalk@gmail.com>
2018-04-03 20:09:27 +0300

    basic version of 0.2, without functionalities

--- main/WBKLogsParser.py
+++ main/WBKLogsParser.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 
 def utilDistinct(listArg):
     return(list(set(listArg)))
@@ -10,6 +11,17 @@ def readFile(filename):
             listOfLines.append(line)
     return listOfLines
 
+def readCSVFile(filename):
+    rows = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            rows.append(row)
+    return rows[1:]
+
+def parseTaskName(taskname):
+    return taskname[:5].upper()+'_'+taskname[-1]
+
 def chooseCandidates(lines):
     result = []
     keyword = 'perform'
@@ -18,6 +30,31 @@ def chooseCandidates(lines):
             result.append(line)
     return result
 
+def chooseCandidatesWithTime(lines):
+    result = []
+    keyword1 = 'STARTED'
+    keyword2 = 'COMPLETED'
+    for line in lines:
+        if keyword1 in line or keyword2 in line:
+            result.append(line)
+    return result
+
+def getTypeNameTime(line):
+    items = line.split(' ')
+
+    type = -1
+    if items[3] == 'STARTED':
+        type = 0
+    elif items[3] == 'COMPLETED':
+        type = 1
+
+    name = parseTaskName(items[-1].split('_')[0])
+
+    date = items[-1].split('_')[1].split('-')
+    time = str(int(date[3])*3600+int(date[4])*60+int(date[5]))
+
+    return type, name, time
+
 def parseLinesToNames(candidates):
     result = []
     for line in candidates:
@@ -25,20 +62,33 @@ def parseLinesToNames(candidates):
         result.append(items[1])
     return result
 
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("inputFile", help="Address of input log file", type=str)
+    parser.add_argument("dependenciesFile", help="Address of input log file", type=str)
     args = parser.parse_args()
 
     lines = readFile(args.inputFile)
 
-    candidates = chooseCandidates(lines)
+    csv = readCSVFile(args.dependenciesFile)
 
-    tasks = utilDistinct(parseLinesToNames(candidates))
+    for line in csv:
+        print(line)
+
+    candidates = chooseCandidatesWithTime(lines)
+
+    tasks = utilDistinct(parseLinesToNames(chooseCandidates(lines)))
 
 
-    for name in tasks:
-        print(name)
+    for task in tasks:
+        print(task)
+
+
+    for line in candidates:
+        print(getTypeNameTime(line))
+        print(line)
 
 
 if __name__ == "__main__":
