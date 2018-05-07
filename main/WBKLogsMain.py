729ac1094cd2c95b4564dfb5af522b52aac16507
Filip Nienartowicz <filip.nienartowicz@student.put.poznan.pl>
2018-05-07 15:01:06 +0200

    Select of n last logs to be parsed Script with comments

--- main/WBKLogsMain.py
+++ main/WBKLogsMain.py
@@ -1,31 +1,58 @@
 import argparse
-import csv
-import WBKLogsParser
-from os import listdir
+import traceback
+from datetime import datetime
 import glob
-import os
+import re
 
+#script with parser
+import WBKLogsParser
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("planname", help="Name of the plan", type=str)
-    parser.add_argument("inputDirectory", help="Address of input log files directory", type=str)
-    parser.add_argument("numOfLogs", help="Number of logs to be analyzed", type=int)
-    parser.add_argument("dependenciesFile", help="Address of input dependencies csv file", type=str)
-    args = parser.parse_args()
+#Select last countOfLogs logs to be analayzed and starts the process
+#
+#Arguments:
+#planname - Name of the plan
+#inputDirectory - Directory where log files are storaged
+#countOfLogs - Count of logs to be analyzed
+#dependenciesFile - Address of input dependencies csv file
+def main(planname, inputDirectory, countOfLogs, dependenciesFile):
+    logs = glob.glob(inputDirectory + '/' + planname + '*.log')
 
-    logs = glob.glob(args.inputDirectory + '/' + args.planname + '*')
+    logsdates = []
+    for i in range(len(logs)):
+        #Extract datetime part of log file name
+        match = re.search(r'\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}', logs[i])
+        #makes tuple made of log file name and date time of it's perform
+        logsdates.append([logs[i],datetime.strptime(match.group(), '%Y-%m-%d-%H-%M-%S')])
 
-    logs = sorted(logs, key=os.path.getctime, reverse=True)
+    #sorts logs due to datetime (descending)
+    logsdates = sorted(logsdates, key=lambda logdate: logdate[1], reverse=True)
 
-    logs = logs[:args.numOfLogs]
+    #select countOfLogs last logs
+    logsdates = logsdates[:countOfLogs]
 
-    for log in logs:
-        log = log.replace("\\", "/")
+    #starts function to parse logs into csv files
+    for log in logsdates:
+        #todo - only Windows should need it - can be removed at the end
+        log[0] = log[0].replace("\\", "/")
         print('')
         print('Output for log:')
-        print(log)
-        WBKLogsParser.main(log, args.dependenciesFile)
+        print(log[0])
+        try:
+            WBKLogsParser.main(log[0], dependenciesFile)
+        except:
+            traceback.print_exc()
 
+#Extracts arguments from args and starts main program
+#args:
+#planname - Name of the plan
+#inputDirectory - Directory where log files are storaged
+#countOfLogs - Count of logs to be analyzed
+#dependenciesFile - Address of input dependencies csv file
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("planname", help="Name of the plan", type=str)
+    parser.add_argument("inputDirectory", help="Directory where log files are storaged", type=str)
+    parser.add_argument("countOfLogs", help="Count of logs to be analyzed", type=int)
+    parser.add_argument("dependenciesFile", help="Address of input dependencies csv file", type=str)
+    args = parser.parse_args()
+    main(args.planname, args.inputDirectory, args.numOfLogs, args.dependenciesFile)
